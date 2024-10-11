61225d063e9ad5d5941a7648080423d816d2ea51
IsakBH <89648535+IsakBH@users.noreply.github.com>
2024-10-11 16:00:43 +0200

    Oppdater shell reload scriptet
    Byttet notification manager, lagt til restart av xdg-desktop portals og lagt til en dock

--- shell_reload.sh
+++ shell_reload.sh
@@ -3,12 +3,37 @@ killall waybar
 
 waybar
 
-#restart dunst
-killall dunst
+# restart notification daemon (swaync)
+killall swaync
 
-dunst
+swaync
 
 #restart hyprpaper
 killall hyprpaper
 
 hyprpaper
+
+sleep 1
+
+# kill all possible running xdg-desktop-portals
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gnome
+killall -e xdg-desktop-portal-kde
+killall -e xdg-desktop-portal-lxqt
+killall -e xdg-desktop-portal-wlr
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+sleep 1
+
+# start xdg-desktop-portal-hyprland
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
+
+# start xdg-desktop-portal
+/usr/lib/xdg-desktop-portal &
+sleep 1
+
+# restart dock
+killall nwg-dock-hyprland
+
+nwg-dock-hyprland -d
